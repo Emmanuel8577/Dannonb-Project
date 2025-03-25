6690839466dd443e5fef1f51c15b133f60e0284f
Emmanuel Adikwu <emmanueledache54@gmail.com>
2025-03-25 15:44:19 +0100

    updated setting.py file

--- dannomb/settings.py
+++ dannomb/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = 'django-insecure-pii(^phj8r7e5p%3272*42-3g!n*2te$w-t1s)z*e@d)!6i+m9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["dannonb-project.onrender.com",
-    "127.0.0.1",]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -124,15 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
-# STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 #EMAIL_BACKEND = env.str("EMAIL_BACKEND")
